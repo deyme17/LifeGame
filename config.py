e860153f92416c201b1c999ab6260616d4591c2c
dmytro <dmytro19022006@gmail.com>
2025-07-27 00:19:03 +0300

    game config update

--- config.py
+++ config.py
@@ -2,13 +2,17 @@ WIDTH, HEIGHT = 1000, 800
 
 CELL_SIZE = 10
 GRID_SIZE = (WIDTH // CELL_SIZE, HEIGHT // CELL_SIZE)
+SHOW_GRID = True
 
 FPS = 10
 
 ALIVE_COLOR = (50, 205, 50)     # Lime
 DEAD_COLOR = (40, 40, 40)       # Gray
 GRID_COLOR = (0, 0, 0)          # Black
+BACKGROUND_COLOR = DEAD_COLOR
 
 # Rules
 BIRTH = {3}                # A dead cell with 3 neighbors becomes alive
 SURVIVE = {2, 3}           # A live cell with 2 or 3 neighbors survives
+INITIAL_FILL_PROBABILITY = 0.2
+INITIAL_STATE = 'random'
