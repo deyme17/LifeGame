c2788f1013df9a933bb2ada1cb0ab6796d0ecdd5
dmytro <dmytro19022006@gmail.com>
2025-07-27 14:35:45 +0300

    game with settings

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from core import Grid, RandomCellGenerator, Game
-from ui.renderers import GridRenderer
+from ui import GridRenderer, SettingsPanel
 from utils.settings import Settings
-from config import WIDTH, HEIGHT
+from config import WIDTH, HEIGHT, SETTINGS_PANEL_SIZE
 import pygame as pg
 
 pg.init()
 
 def main() -> None:
     settings = Settings()
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen = pg.display.set_mode((WIDTH + SETTINGS_PANEL_SIZE, HEIGHT))
 
     renderer = GridRenderer(screen, (settings.GRID_WIDTH, settings.GRID_HEIGHT), settings.CELL_SIZE)
     grid = Grid((settings.GRID_WIDTH, settings.GRID_HEIGHT), settings.SURVIVE, settings.BIRTH)
     cell_gen = RandomCellGenerator((settings.GRID_WIDTH, settings.GRID_HEIGHT))
 
-    game = Game(settings, renderer, grid, cell_gen)
+    settings_panel = SettingsPanel(screen, WIDTH, 0, SETTINGS_PANEL_SIZE, HEIGHT, settings)
+
+    game = Game(settings, renderer, grid, cell_gen, settings_panel)
     game.start_game()
 
 if __name__=='__main__':
